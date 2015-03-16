70e52051b036a62b090af2c7826ebf2a15292fcd
willemwouters@gmail.com <willemwouters@gmail.com>
2015-03-16 18:13:21 +0100

    added examples, and removed some invalid relative paths in include

---
+++ projects/examples/udp_send/build/src/udp_transmit.d
@@ -0,0 +1,95 @@
+build/src/udp_transmit.o: src/udp_transmit.c bsp/include/wiced.h \
+ bsp/include/wiced_utilities.h bsp/include/wiced_result.h \
+ bsp/include/WWD/wwd_constants.h \
+ bsp/arm/bin/Linux64//../../include/stdint.h \
+ bsp/arm/bin/Linux64//../../include/_ansi.h \
+ bsp/arm/bin/Linux64//../../include/newlib.h \
+ bsp/arm/bin/Linux64//../../include/sys/config.h \
+ bsp/arm/bin/Linux64//../../include/machine/ieeefp.h \
+ bsp/arm/bin/Linux64//../../include/sys/features.h \
+ bsp/include/WWD/wwd_wlioctl.h bsp/include/wiced_resource.h \
+ /home/wouters/GIT/willemwouters/EMW3162_WICED/projects/../WICED_SRC/WICED/security/BESL/include/besl_structures.h \
+ bsp/include/platform/platform_constants.h bsp/include/wiced_constants.h \
+ bsp/include/../libraries/bluetooth/include/wiced_bt_constants.h \
+ bsp/include/WWD/wwd_structures.h bsp/include/WWD/wwd_constants.h \
+ bsp/arm/bin/Linux64//../../include/stdlib.h \
+ bsp/arm/bin/Linux64//../../include/_ansi.h \
+ bsp/arm/bin/Linux64//../../lib/include/stddef.h \
+ bsp/arm/bin/Linux64//../../include/sys/reent.h \
+ bsp/arm/bin/Linux64//../../include/sys/_types.h \
+ bsp/arm/bin/Linux64//../../include/machine/_types.h \
+ bsp/arm/bin/Linux64//../../include/machine/_default_types.h \
+ bsp/arm/bin/Linux64//../../include/sys/lock.h \
+ bsp/arm/bin/Linux64//../../include/machine/stdlib.h \
+ bsp/arm/bin/Linux64//../../include/alloca.h \
+ bsp/include/libraries/ring_buffer.h bsp/include/wiced_framework.h \
+ bsp/include/platform/platform_dct.h bsp/include/WWD/wwd_wifi.h \
+ bsp/include/WWD/internal/chips/chip_constants.h \
+ bsp/include/WWD/wwd_wlioctl.h bsp/include/WWD/RTOS/wwd_rtos_interface.h \
+ bsp/include/RTOS/FreeRTOS/wwd_rtos.h \
+ bsp/include/platform/ARM_CM3/platform_isr.h \
+ bsp/include/RTOS/FreeRTOS/FreeRTOS.h \
+ bsp/include/RTOS/FreeRTOS/projdefs.h \
+ bsp/include/RTOS/FreeRTOS/ARM_CM3/FreeRTOSConfig.h \
+ bsp/include/MXCHIP3162/platform_config.h \
+ bsp/include/RTOS/FreeRTOS/wwd_FreeRTOS_systick.h \
+ bsp/include/WWD/wwd_assert.h bsp/include/WWD/wwd_debug.h \
+ bsp/include/wiced_defaults.h bsp/arm/bin/Linux64//../../include/stdio.h \
+ bsp/arm/bin/Linux64//../../lib/include/stdarg.h \
+ bsp/arm/bin/Linux64//../../include/sys/types.h \
+ bsp/arm/bin/Linux64//../../include/machine/types.h \
+ bsp/arm/bin/Linux64//../../include/sys/stdio.h \
+ bsp/include/platform/ARM_CM3/platform_assert.h \
+ bsp/include/WWD/platform/wwd_bus_interface.h \
+ bsp/include/WWD/network/wwd_buffer_interface.h \
+ bsp/include/network/lwip/wwd_buffer.h \
+ bsp/include/WWD/internal/wwd_bus_protocol.h \
+ bsp/include/WWD/platform/wwd_sdio_interface.h \
+ bsp/include/RTOS/FreeRTOS/portable.h \
+ bsp/include/RTOS/FreeRTOS/ARM_CM3/portmacro.h \
+ bsp/include/RTOS/FreeRTOS/mpu_wrappers.h \
+ bsp/include/RTOS/FreeRTOS/semphr.h bsp/include/RTOS/FreeRTOS/queue.h \
+ bsp/include/RTOS/FreeRTOS/task.h bsp/include/RTOS/FreeRTOS/list.h \
+ bsp/include/RTOS/FreeRTOS/wwd_FreeRTOS_systick.h \
+ bsp/include/WWD/network/wwd_network_interface.h \
+ bsp/include/wiced_platform.h bsp/include/platform/platform_peripheral.h \
+ bsp/include/periph/platform_mcu_peripheral.h \
+ bsp/include/periph/platform_cmsis.h bsp/include/periph/stm32f2xx.h \
+ bsp/include/periph/core_cm3.h bsp/include/periph/core_cmInstr.h \
+ bsp/include/periph/core_cmFunc.h bsp/include/periph/system_stm32f2xx.h \
+ bsp/include/periph/stm32f2xx_adc.h bsp/include/periph/stm32f2xx_dma.h \
+ bsp/include/periph/stm32f2xx_exti.h bsp/include/periph/stm32f2xx_flash.h \
+ bsp/include/periph/stm32f2xx_gpio.h bsp/include/periph/stm32f2xx_i2c.h \
+ bsp/include/periph/stm32f2xx_iwdg.h bsp/include/periph/stm32f2xx_pwr.h \
+ bsp/include/periph/stm32f2xx_rtc.h bsp/include/periph/stm32f2xx_syscfg.h \
+ bsp/include/periph/stm32f2xx_tim.h bsp/include/periph/stm32f2xx_usart.h \
+ bsp/include/WWD/RTOS/wwd_rtos_interface.h \
+ bsp/include/MXCHIP3162/platform.h bsp/include/wiced_rtos.h \
+ bsp/include/RTOS/FreeRTOS/rtos.h bsp/include/RTOS/FreeRTOS/timers.h \
+ bsp/include/RTOS/FreeRTOS/wwd_rtos.h \
+ bsp/include/WWD/platform/wwd_platform_interface.h \
+ bsp/include/wiced_tcpip.h \
+ bsp/include/WWD/network/wwd_network_interface.h \
+ bsp/include/network/lwip/wiced_network.h \
+ bsp/include/RTOS/FreeRTOS/FreeRTOS.h bsp/include/RTOS/FreeRTOS/task.h \
+ bsp/arm/bin/Linux64//../../include/string.h \
+ bsp/arm/bin/Linux64//../../include/sys/cdefs.h \
+ bsp/arm/bin/Linux64//../../include/sys/string.h \
+ bsp/include_security/BESL/host/WICED/tls_types.h \
+ bsp/include_security/BESL/crypto/crypto_structures.h \
+ bsp/arm/bin/Linux64//../../include/time.h \
+ bsp/arm/bin/Linux64//../../include/machine/time.h \
+ bsp/include_security/BESL/host/WICED/cipher_suites.h \
+ bsp/include/network/lwip/ipv4/lwip/ip_addr.h \
+ bsp/include/network/lwip/lwip/opt.h bsp/include/network/lwip/lwipopts.h \
+ bsp/include/WWD/network/wwd_network_constants.h \
+ bsp/include/WWD/network/wwd_buffer_interface.h \
+ bsp/include/network/lwip/lwip/debug.h \
+ bsp/include/network/lwip/lwip/arch.h bsp/include/network/lwip/arch/cc.h \
+ bsp/include/network/lwip/cpu.h bsp/include/network/lwip/lwip/def.h \
+ bsp/include/network/lwip/lwip/netif.h \
+ bsp/include/network/lwip/lwip/err.h bsp/include/network/lwip/lwip/pbuf.h \
+ bsp/include/wiced_wifi.h bsp/arm/bin/Linux64//../../include/limits.h \
+ bsp/arm/bin/Linux64//../../lib/include-fixed/limits.h \
+ bsp/include/wiced_time.h bsp/include/wiced_management.h \
+ bsp/include/wiced_easy_setup.h bsp/include/WWD/wwd_debug.h
