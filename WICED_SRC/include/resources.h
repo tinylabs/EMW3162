11d5f2d05c06e6a8d71785899c162ed5efbfef72
willemwouters@gmail.com <willemwouters@gmail.com>
2015-03-16 17:56:18 +0100

    added 2 udp sample projects

--- WICED_SRC/include/resources.h
+++ WICED_SRC/include/resources.h
@@ -1,11 +1,3 @@
 /* Auto-generated header file. Do not edit */
 #ifndef INCLUDED_RESOURCES_H_
 #define INCLUDED_RESOURCES_H_
-#include "wiced_resource.h"
-
-extern const resource_hnd_t build_resources_index_html_c;
-extern const char build_resources_index_html_c_data[6];
-extern const resource_hnd_t build_resources_test_png_c;
-extern const char build_resources_test_png_c_data[1863];
-
-#endif /* ifndef INCLUDED_RESOURCES_H_ */
---
+++ projects/udp_receive/build/dct/dct.d
@@ -0,0 +1,47 @@
+build/dct/dct.o: src/dct.c bsp/include/MXCHIP3162/platform_config.h \
+ bsp/include/generated_security_dct.h bsp/include/platform/platform_dct.h \
+ bsp/arm/bin/Linux64//../../include/stdint.h \
+ bsp/arm/bin/Linux64//../../include/_ansi.h \
+ bsp/arm/bin/Linux64//../../include/newlib.h \
+ bsp/arm/bin/Linux64//../../include/sys/config.h \
+ bsp/arm/bin/Linux64//../../include/machine/ieeefp.h \
+ bsp/arm/bin/Linux64//../../include/sys/features.h \
+ bsp/include/WWD/wwd_wifi.h bsp/include/WWD/wwd_constants.h \
+ bsp/include/WWD/wwd_wlioctl.h \
+ bsp/include/WWD/internal/chips/chip_constants.h \
+ bsp/include/WWD/wwd_wlioctl.h bsp/include/WWD/RTOS/wwd_rtos_interface.h \
+ bsp/include/RTOS/FreeRTOS/wwd_rtos.h \
+ bsp/include/platform/ARM_CM3/platform_isr.h \
+ bsp/include/platform/platform_constants.h \
+ bsp/include/RTOS/FreeRTOS/FreeRTOS.h \
+ bsp/arm/bin/Linux64//../../lib/include/stddef.h \
+ bsp/include/RTOS/FreeRTOS/projdefs.h \
+ bsp/include/RTOS/FreeRTOS/ARM_CM3/FreeRTOSConfig.h \
+ bsp/include/RTOS/FreeRTOS/wwd_FreeRTOS_systick.h \
+ bsp/include/WWD/wwd_assert.h bsp/include/WWD/wwd_debug.h \
+ bsp/include/wiced_defaults.h bsp/arm/bin/Linux64//../../include/stdio.h \
+ bsp/arm/bin/Linux64//../../include/_ansi.h \
+ bsp/arm/bin/Linux64//../../lib/include/stdarg.h \
+ bsp/arm/bin/Linux64//../../include/sys/reent.h \
+ bsp/arm/bin/Linux64//../../include/sys/_types.h \
+ bsp/arm/bin/Linux64//../../include/machine/_types.h \
+ bsp/arm/bin/Linux64//../../include/machine/_default_types.h \
+ bsp/arm/bin/Linux64//../../include/sys/lock.h \
+ bsp/arm/bin/Linux64//../../include/sys/types.h \
+ bsp/arm/bin/Linux64//../../include/machine/types.h \
+ bsp/arm/bin/Linux64//../../include/sys/stdio.h \
+ bsp/include/platform/ARM_CM3/platform_assert.h \
+ bsp/include/WWD/platform/wwd_bus_interface.h \
+ bsp/include/WWD/network/wwd_buffer_interface.h \
+ bsp/include/network/lwip/wwd_buffer.h bsp/include/WWD/wwd_constants.h \
+ bsp/include/WWD/internal/wwd_bus_protocol.h \
+ bsp/include/WWD/platform/wwd_sdio_interface.h \
+ bsp/include/RTOS/FreeRTOS/portable.h \
+ bsp/include/RTOS/FreeRTOS/ARM_CM3/portmacro.h \
+ bsp/include/RTOS/FreeRTOS/mpu_wrappers.h \
+ bsp/include/RTOS/FreeRTOS/semphr.h bsp/include/RTOS/FreeRTOS/queue.h \
+ bsp/include/RTOS/FreeRTOS/task.h bsp/include/RTOS/FreeRTOS/list.h \
+ bsp/include/RTOS/FreeRTOS/wwd_FreeRTOS_systick.h \
+ bsp/include/WWD/wwd_structures.h \
+ bsp/include/WWD/network/wwd_network_interface.h src/wifi_config_dct.h \
+ bsp/include/generated_mac_address.txt
